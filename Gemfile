d96f43426e8cb0dd8a4b82125fbc1805044241f0
Thanh an Huynh <thanh1606@gmail.com>
2018-02-16 15:21:03 +0100

    Add figaro - Protect my secret data in config/application.yml

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+# cloudinary
+gem 'figaro'
 
 # Asset pipeline
 gem "sprockets-rails"
--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 function loadDynamicBannerText() {
   new Typed('#banner-typed-text', {
-    strings: ["Change your life", "Learn to code"],
+    strings: ["Radical Inclusion", "Radical Self-reliance", "Leaving No Trace", "Civic Responsibility"],
     typeSpeed: 50,
     loop: true
   });
